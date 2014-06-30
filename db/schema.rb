918dbdd1578b8cfd70e98d98b7e2470d877fe383
newesish <hugo@tbf.mx>
2014-06-30 17:54:13 -0500

    se elimino la opcion de renta y venta en la tambla y se dejo un precio unico al igual que se actualizo en las vistas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140630181145) do
+ActiveRecord::Schema.define(version: 20140630223311) do
 
   create_table "brands", force: true do |t|
     t.string   "name"
@@ -59,18 +59,16 @@ ActiveRecord::Schema.define(version: 20140630181145) do
     t.text     "description"
     t.integer  "publication_type"
     t.integer  "category_id"
-    t.decimal  "precio_venta"
-    t.decimal  "precio_renta"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "image_id"
     t.integer  "user_id"
     t.integer  "modelo"
-    t.string   "moneda_renta"
-    t.string   "moneda_venta"
     t.string   "pais"
     t.string   "estado"
     t.string   "ciudad"
+    t.decimal  "precio"
+    t.string   "moneda"
   end
 
   create_table "galleries", force: true do |t|
