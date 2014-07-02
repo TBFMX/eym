1ee17380df21a369e27aaf3ed85aa829baa78ef3
Salvador Teran <chava@tbf.mx>
2014-07-02 16:13:30 -0500

    pruebas a descuentos y validaciones

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,3 +1,5 @@
 class Discount < ActiveRecord::Base
   belongs_to :equipment
+  validates :titulo, presence: true, uniqueness: true
+  validates :precio, :vencimiento, :equipment_id, presence: true
 end
