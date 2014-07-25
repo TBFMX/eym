e290d6f82ad6c91b97fed40e9dcf65cf107fdeae
Sebastian-tbf <Sebastian@tbf.mx>
2014-07-25 11:24:30 -0500

    ajustes login externos

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,11 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 	
 	#provider :twitter, ENV['G7wFqd9MDPhvIUVGu9T5nVBdF'], ENV['ot9gPzMWUGQMuRafg5vxAwWpFDVIVMLHSitR8uie1JAMJsuXl0']
 	provider :google_oauth2, '897257467068-npulc9dqjavi23kmp2sli8pn1vuppuhs.apps.googleusercontent.com', '4BMlC5_QjUh2JDA90UanCMu-'
+	#localhost
 	provider :facebook, '1451557391770177', '3607dfa33038469e7d37bd1b60961343'
+	#provider :facebook, '281783672006763', '6a63c1628abe7157f9d673c975bad6aa'
+	#produccion
+	#provider :facebook, '725899564148428', 'fecb9b6639957af381cbc55db2005c0f'
 	#provider :identity, on_failed_registration: lambda { |env|    UsersController.action(:new).call(env)}
 end
 
