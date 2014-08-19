ec4c4c95760717d2168dd961d0cea60839abd291
Sebastian-tbf <Sebastian@tbf.mx>
2014-08-19 10:05:22 -0500

    nuevo login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,9 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def new_en
+  end
+
   def createfb
   
 
@@ -60,6 +63,35 @@ class SessionsController < ApplicationController
   		redirect_to root_path, alert: "Invalid user/password combination"
   	end
   end
+
+  def create_en
+    user = User.find_by(username: params[:username])
+    if user and user.authenticate(params[:password])
+      session[:user_id] = user.id
+      session[:uname] = params[:username]
+      session[:rol_id] = user.rol_id
+      #variables de rol
+      rol =Rol.find_by(id: user.rol_id)
+      session[:mod0] =rol.admin
+      session[:mod1] =rol.module_1
+      session[:mod2] =rol.module_2
+      session[:mod3] =rol.module_3
+      session[:mod4] =rol.module_4
+      session[:mod5] =rol.module_5
+      #termina variables de rol
+      unless session[:lasurl].blank?
+        aux = session[:lasurl]
+        session[:lasurl] = ""
+        redirect_to aux
+      else
+        redirect_to "/equipment/new"
+      end    
+    else
+      redirect_to root_path, alert: "La combinacion de Usuario y contraseña es incorrecta"
+    end
+  end
+
+
 
   def destroy
   	#session[:user_id] = nil
