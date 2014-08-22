6de41ff33c36c9274c5cdc052b24d3eaea69ff67
chava-tbf <chava@tbf.mx>
2014-08-22 09:34:03 -0500

    borrar linea de application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,5 +32,4 @@
     js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=<%=OORConstant.facebook_app_id%>";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk')
-window.alert("active el fb");
   );
