bdfa54a78e9d463f247c7adf1fbb0c8bfdac3c83
newesish <hugo@tbf.mx>
2014-08-25 15:57:20 -0500

    se modifico el estilo de los grid

--- app/assets/javascripts/mostrar_ocultar.js
+++ app/assets/javascripts/mostrar_ocultar.js
@@ -1,14 +1,14 @@
-	function mostrar(elem_id,botton) {
-		var miDiv, mostrar;
-		miDiv = document.getElementById(elem_id);
-		if(miDiv.style.display == "none") {
-			miDiv.style.display = "block";
-			mostrar = document.getElementById(botton).childNodes[0];
-			mostrar.data = "Ocultar";
-		}
-		else {
-			miDiv.style.display = "none";
-			mostrar = document.getElementById(botton).childNodes[0];
-			mostrar.data = "Mostrar";
-		}
-	}
+    function mostrar(elem_id,botton) {
+        var miDiv, mostrar;
+        miDiv = document.getElementById(elem_id);
+        if(miDiv.style.display == "none") {
+            miDiv.style.display = "block";
+            mostrar = document.getElementById(botton).childNodes[0];
+            mostrar.data = printf("&#9650;");
+        }
+        else {
+            miDiv.style.display = "none";
+            mostrar = document.getElementById(botton).childNodes[0];
+            mostrar.data = printf("&#9660;");
+        }
+    }
